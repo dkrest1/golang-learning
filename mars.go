7e56d6b775c60d6f9795da6b6b875652e8863e3c
Akande Oluwatosin <aktosin1@gmail.com>
2023-06-30 10:06:21 +0100

    update: var, const, switch, in go

--- mars.go
+++ mars.go
@@ -1,12 +1,61 @@
-// weight loss program
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main () {
-	const hoursPerday = 24
-	var speed, distance = 100800, 9630000 
-	
-	fmt.Println(distance/speed/hoursPerday, "days")
-	
+	// countDown()
+	// infinity()
+	exercise3()
+}
+
+//countdown using the for loop function
+func countDown () {
+	var count  = 10
+
+	for count > 0 {
+		fmt.Println(count)
+		time.Sleep(time.Second)
+		count--
+	}
+
+	fmt.Println("Liftoff!")
+}
+
+// to infinity and beyound 
+func infinity() {
+	var degrees = 0
+	for {
+		fmt.Println(degrees)
+		degrees++
+		if(degrees >= 360) {
+			degrees = 0
+			if(rand.Intn(2) == 0) {
+				fmt.Println("I am going light off right now!")
+				break
+			}
+		}
+	}
+}
+
+// exercise three 
+func exercise3 () {
+	var count = 10
+	for count > 0 {
+		fmt.Println(count)
+		time.Sleep(time.Second)
+		if(rand.Intn(100) == 0) {
+			break
+		}
+		count--
+	}
+
+	if(count == 0) {
+		fmt.Println("Liftoff!")
+	}else {
+		fmt.Println("Launch failed.")
+	}
 }
